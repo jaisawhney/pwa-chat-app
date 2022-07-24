01ba99f7471310a9f1c733e134a31a2f64e2b0e9
Jai Sawhney <jai.sawhney1@gmail.com>
2022-07-24 04:44:31 -0700

    Add a serviceWorker

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use('/static', express.static('static'))
+
+app.get('/static/serviceWorker.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static/serviceWorker.js'), {
+        headers: {
+            'Service-Worker-Allowed': '/'
+        }
+    });
+});
+
+app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
     res.render('index');
---
+++ static/serviceWorker.js
@@ -0,0 +1,31 @@
+const cacheName = 'pwa-chat-app'
+const files = [
+    '/',
+    '/static/js/main.js'
+]
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(files);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request);
+        })
+    )
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.filter(key => key !== cacheName).map(key => {
+                return caches.delete(key);
+            }))
+        })
+    )
+});
