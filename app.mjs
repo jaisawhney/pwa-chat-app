04a469ab7cd1e560042447bd272295606583d4eb
Jai Sawhney <jai.sawhney1@gmail.com>
2022-07-26 18:51:20 -0700

    Implement websocket broadcast and rooms

--- app.mjs
+++ app.mjs
@@ -1,14 +1,66 @@
 import express from 'express';
 import http from 'http';
 import {WebSocket, WebSocketServer} from 'ws'
+import crypto from 'crypto';
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({server});
 
-wss.on("connection", ws => {
-    ws.on("message", payload => {
+const rooms = {};
+wss.on('connection', ws => {
+    const broadcast = (payload, roomId) => {
+        for (const client of rooms[roomId] || wss.clients) {
+            if (client.readyState === WebSocket.OPEN && client !== ws) {
+                client.send(payload);
+            }
+        }
+    }
 
+    const createRoom = () => {
+        const roomId = crypto.randomUUID();
+
+        rooms[roomId] = [ws];
+        ws.room = roomId;
+        return roomId
+    }
+
+    const joinRoom = (params) => {
+        const roomId = params.roomId
+
+        if (!rooms[roomId]) return;
+        if (!rooms[roomId][ws]) return;
+
+        rooms[roomId].push(ws);
+        ws.room = roomId;
+    }
+
+    const leaveRoom = (params) => {
+        const roomId = params.roomId
+
+        if (rooms[roomId].length === 1) {
+            delete rooms[roomId]
+        } else {
+            delete rooms[roomId][ws]
+        }
+    }
+
+    ws.on('message', data => {
+        const payload = JSON.parse(data);
+
+        switch (payload.type) {
+            case 'joinRoom':
+                joinRoom(payload.params);
+                break;
+
+            case 'leaveRoom':
+                leaveRoom(payload.params);
+                break;
+
+            case 'message':
+                broadcast(JSON.stringify(payload));
+                break;
+        }
     });
 });
 
